b0b66a81c2421ca4a022b00158867f9e1e62ee86
Yogesh Pal <yogeshpal5049@gmail.com>
2024-12-28 11:59:02 +0530

    mobile banner update

--- components/HeroSchlore.jsx
+++ components/HeroSchlore.jsx
@@ -1,34 +1,28 @@
-
 import React from "react";
 import Image from "next/image";
 
 const HeroSchlore = () => {
   return (
     <>
-    
-    <div className="mt-20    w-full h-full hidden md:block">
-      <Image
-        src="/11.png"
-        alt="School choice"
-        width={1000}
-        height={1000}
-        className="w-full h-full "
+      <div className="mt-20    w-full h-full hidden md:block">
+        <Image
+          src="/11.png"
+          alt="School choice"
+          width={1000}
+          height={1000}
+          className="w-full h-full "
         />
-
-      
-    </div>
-    <div className="w-full h-auto md:hidden">
-      <Image
-        src="/mobilebg.svg"
-        alt="School choice"
-        width={1000}
-        height={1000}
-        className="w-full h-auto "
+      </div>
+      <div className="w-full h-auto md:hidden">
+        <Image
+          src="/5.png"
+          alt="School choice"
+          width={1000}
+          height={1000}
+          className="w-full h-auto "
         />
-
-      
-    </div>
-        </>
+      </div>
+    </>
   );
 };
 
